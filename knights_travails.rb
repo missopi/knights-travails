111e40a85e5ce7b9e2c822dce789d3fe7d0049de
missopi <missopi@me.com>
2023-08-10 15:36:45 +0100

    Add some notes from exercise

--- knights_travails.rb
+++ knights_travails.rb
@@ -3,3 +3,16 @@ KNIGHT_POSSIBLE_MOVES = [x+1, y+2], [x+1, y-2], [x+2, y-1], [x+2, y+1],
 
 def knight_moves(x, y)
 end
+
+# create game board - ([8, 8])
+# create knight to 'move' around board
+# knight moves = tree children
+# choose search algorythm
+# use to find shortest path  between start and end position of knight
+# output full path 
+# knight_moves([3,3],[4,3])
+# You made it in 3 moves!  Here's your path:
+# [3,3]
+# [4,5]
+# [2,4]
+# [4,3]
