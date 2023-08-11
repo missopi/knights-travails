881a4392897722daba04fa0da03dc95feaecfdc8
missopi <missopi@me.com>
2023-08-11 12:44:21 +0100

    Add notes

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,48 +1,57 @@
 # frozen_string_literal: true
 
+# class for the game tree
 class GameTree
-    # KNIGHT_POSSIBLE_MOVES = [[x+1, y+2], [x+1, y-2], [x+2, y-1], [x+2, y+1], 
-    #    [x-1, y+2], [x-1, y-2], [x-2, y-1], [x-2, y+1]]
+  POSSIBLE_MOVES = [[1, 2], [1, -2], [2, -1], [2, 1], [-1, 2], [-1, -2],
+                    [-2, -1], [-2, 1]].freeze
 
-    def initialize
-    end
+  def initialize
+  end
 
-    def create_board
-        @board = Array.new(8) { Array.new(8) }
-    end
+  def create_board
+    @board = Array.new(8) { Array.new(8) }
+  end
 
-    def valid_move
-    end
+  def valid_move
+    # return true if the move is still on the game board
+  end
 
-    def knight_moves(x, y)
-        knight = KnightNode.new
-    end
+  def knight_moves(start, finish)
+  end
 
-    def number_of_moves
-    end
+  def create_move_list
+    # create array of previous moves made
+  end
 
-    def print_result
-        puts "You made it in #{} moves! Here's your path: "
-        puts "#{knight_moves([x, y])}"
-    end
+  def number_of_moves
+    # add up number of elements in move list
+    # moves = integer
+  end
+
+  def print_result
+    puts "You made it in #{moves} moves! Here's your path: "
+    # puts full path of moves from create_move_list method
+    # [3,3]
+    # [4,5]
+    # [2,4]
+    # [4,3]
+  end
 end
 
+# class for node
 class KnightNode
-    
+  def initialize
+    # record positiopn knight is in [x, y]
+    # record previous node which it came from (for path)
+    # record if node has position has been visited before
+  end
 end
 
 chess = GameTree.new
 p chess.create_board
 
-# create knight to 'move' around board
 # knight moves = tree children
 # use breadth first search algorythm
 # use to find shortest path  between start and end position of knight
-# output full path 
-# knight_moves([3,3],[4,3])
-# You made it in 3 moves!  Here's your path:
-# [3,3]
-# [4,5]
-# [2,4]
-# [4,3]
+
 # use recursion
