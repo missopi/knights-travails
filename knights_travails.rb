f79d3ff0177374e48c69c8e5e14d0b7b896a02b7
missopi <missopi@me.com>
2023-08-11 11:49:55 +0100

    Write down some initial ideas for layout etc

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,13 +1,31 @@
-KNIGHT_POSSIBLE_MOVES = [x+1, y+2], [x+1, y-2], [x+2, y-1], [x+2, y+1], 
-                        [x-1, y+2], [x-1, y-2], [x-2, y-1], [x-2, y+1]
+# frozen_string_literal: true
 
-def knight_moves(x, y)
+class GameTree
+    KNIGHT_POSSIBLE_MOVES = [[x+1, y+2], [x+1, y-2], [x+2, y-1], [x+2, y+1], 
+        [x-1, y+2], [x-1, y-2], [x-2, y-1], [x-2, y+1]]
+
+    def initialize
+    end
+
+    def create_board
+        @board = Array.new
+    end
+
+    def valid_move
+    end
+
+    def knight_moves([x, y])
+    end
 end
 
-# create game board - ([8, 8])
+class KnightNode
+    def initialize
+    end
+end
+
 # create knight to 'move' around board
 # knight moves = tree children
-# choose search algorythm
+# use breadth first search algorythm
 # use to find shortest path  between start and end position of knight
 # output full path 
 # knight_moves([3,3],[4,3])
@@ -16,3 +34,4 @@ end
 # [4,5]
 # [2,4]
 # [4,3]
+# use recursion
