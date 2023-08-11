4b069efc86c8d8df045268f0e91d1f2a6d8beac5
missopi <missopi@me.com>
2023-08-11 15:30:46 +0100

    Move possible_moves and make move list method

--- knights_travails.rb
+++ knights_travails.rb
@@ -2,8 +2,7 @@
 
 # class for the game tree
 class GameTree
-  POSSIBLE_MOVES = [[1, 2], [1, -2], [2, -1], [2, 1], [-1, 2], [-1, -2],
-                    [-2, -1], [-2, 1]].freeze
+  
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
@@ -18,25 +17,23 @@ class GameTree
     valid = valid_move?(start, finish)
     return unless valid
 
-    knight = KnightNode.new(start)
-    queue = [knight]
+    queue = [KnightNode.new(start)]
 
     # until knight.position == finish
     #   create_move_list(finish)
-    #   queue.shift
+    #   current = queue.shift
     # end
-
-    # path = 
 
     # puts "You made it in #{path.size} moves! Here's your path: "
     # puts full path of moves from create_move_list method
     # path.each
   end
-
 end
 
 # class for node
 class KnightNode
+  POSSIBLE_MOVES = [[1, 2], [1, -2], [2, -1], [2, 1], [-1, 2], [-1, -2],
+                    [-2, -1], [-2, 1]].freeze
   attr_accessor :position, :previous, :visited
 
   def initialize(row = nil, column = nil)
@@ -44,11 +41,20 @@ class KnightNode
     @previous = [] # children
     @visited = nil # parent
   end
+
+  def create_move_list
+    move_list = []
+    POSSIBLE_MOVES.each do |move|
+      new_move = [@position[0] + move[0], @position[1] + move[1]]
+      move_list.push(new_move) if valid_move?(new_move)
+    end
+    move_list
+  end
 end
 
 test = GameTree.new
-# test.knight_moves([0, 0], [2, 1])
-test.create_move_list
+p test.knight_moves([0, 0], [2, 1])
+# test.create_move_list
 
 # knight moves = tree children
 # use breadth first search algorythm
