a5e71065df9194843b355e14a1d9a6b5e4b1e0d8
missopi <missopi@me.com>
2023-08-11 16:51:47 +0100

    Fix issue with node array

--- knights_travails.rb
+++ knights_travails.rb
@@ -7,14 +7,13 @@ class GameTree
   end
 
   def knight_moves(start, finish)
-    queue = [KnightNode.new(start)]
+    knight = KnightNode.new(start)
+    queue = [knight]
 
-    # until knight.position == finish
-    #   create_move_list(finish)
-    #   current = queue.shift
-    # end
+  end
 
-    # puts "You made it in #{path.size} moves! Here's your path: "
+  def print
+    puts "You made it in #{path.size} moves! Here's your path: "
     # puts full path of moves from create_move_list method
     # path.each
   end
@@ -26,8 +25,8 @@ class KnightNode
                     [-2, -1], [-2, 1]].freeze
   attr_accessor :position, :previous, :visited
 
-  def initialize(row = nil, column = nil)
-    @position = [row, column]
+  def initialize(position)
+    @position = position
     @previous = [] # children
     @visited = nil # parent
   end
@@ -35,15 +34,14 @@ class KnightNode
   def create_move_list
     move_list = []
     POSSIBLE_MOVES.each do |move|
-      new_move = [@position[0] + move[0], @position[1] + move[1]]
+      new_move = [position[0] + move[0], position[1] + move[1]]
       move_list.push(new_move) if valid_move?(new_move)
     end
     move_list
   end
 
-  def valid_move?(start, finish)
-    return true if start[0].between?(0, 7) && start[1].between?(0, 7)
-    return true if finish[0].between?(0, 7) && finish[1].between?(0, 7)
+  def valid_move?(move)
+    return true if move[0].between?(0, 7) && move[1].between?(0, 7)
   end
 end
 
